e07875c43a2164b2c1528183be62391e4411fe65
victorien <deadvicmangame@gmail.com>
2023-01-26 12:52:10 +0100

    exo 177 php

---
+++ classe/StrUtils.php
@@ -0,0 +1,71 @@
+<?php
+class StrUtils
+{
+public string $nom = 'Bail';
+public string $prenom = 'Victorien';
+public string $email = 'ab@.com';
+public string $password = 'azerty';
+    public function getNom(): string {
+        return $this->nom;
+    }
+
+    /**
+     * @param string $nom
+     */
+    public function setNom(string $nom): void
+    {
+        $this->nom = $nom;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPrenom(): string
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * @param string $prenom
+     */
+    public function setPrenom(string $prenom): void
+    {
+        $this->prenom = $prenom;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     */
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
+
+}
+
+
--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
 
-// TODO n'oubliez pas de créer votre classe dans le dossier classes.
+// TODO oubliette pas de créer votre classe dans le dossier classes.
 
+
+require './classe/StrUtils.php';
+
+include 'StrUtils.php';
+
+$myStr = new StrUtils('php is awesome !');
+?>
